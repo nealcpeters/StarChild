4e888f133ae71539f567324f63498f02c6183a56
Eric Stensland <estensland88@gmail.com>
2014-04-12 14:57:07 -0500

    added composition comparisons

--- app/assets/javascripts/body_composition.js
+++ app/assets/javascripts/body_composition.js
@@ -1,49 +1,89 @@
-var elementalCompositionHumanBody = {
-  "H" : .1 
-  "C" : .18
-  "N" : .3
-  "O" : .65
-  "Na" : .001
-  "K" : .002
-  "P" : .012
-  "Mg" : .0005
-  "S" : .002
-  "Cl" : .002
-  "Ca" : .015
-  "Fe" : .0005
-}
+$(document).ready(function(){
 
-var elementalCompositionMilkyWay = {
-  "H" : .74 
-  "He" : .24
-  "C" : .004
-  "Ne" : .001
-  "O" : .01
-  "Fe" : .001
-  "N" : .001
-  "Si" : .0006
-  "Mg" : .0006
-  "S" : .0004
-}
+  var elementalCompositionHumanBody = {
+    H : 0.1 ,
+    C : 0.18,
+    N : 0.3,
+    O : 0.65,
+    Na : 0.001, //need traces 
+    K : 0.002, //need traces 
+    P : 0.012, //need traces 
+    Mg : 0.0005,
+    S : 0.002,
+    Cl : 0.002,
+    Ca : 0.015,
 
-var elementalCompositionEarth = {
-  "Fe" : .32
-  "O" : .30
-  "Si" : .15
-  "Mg" : .14
-  "S" : .03
-  "Ni" : .018
-  "Ca" : .015
-  "Al" : 014
-}
+    Fe : 0.0005
+  };
 
-var elementalSolarSystem = {
-   "H" : .70 
-  "He" : .275
-  "O" : .006
-  "C" : .003
-  "N" : .001
-  "Ne" : .001
-}
+  var elementalCompositionMilkyWay = {
+    H : 0.74 ,
+    He : 0.24,
+    C : 0.004,
+    Ne : 0.001,
+    O : 0.01,
+    Fe : 0.001,
+    N : 0.001,
+    Si : 0.0006,
+    Mg : 0.0006,
+    S : 0.0004
+  };
 
-// Divide your composition of things by the others to figure out how much more or less percent you have
+  var elementalCompositionEarth = {
+    Fe : 0.32,
+    O : 0.30,
+    Si : 0.15,
+    Mg : 0.14,
+    S : 0.03,
+    Ni : 0.018,
+    Ca : 0.015,
+    Al : 0.014
+  };
+
+  var elementalCompositionSolarSystem = {
+    H : 0.70 ,
+    He : 0.275,
+    O : 0.006,
+    C : 0.003,
+    N : 0.001,
+    Ne : 0.001
+  };
+
+  var hPercentageMilkyWay = elementalCompositionHumanBody.H / elementalCompositionMilkyWay.H * 100 //Percentage
+   $('#hPercentageMilkyWay').find('p').text(hPercentageMilkyWay + "%");
+  var hPercentageSolarSystem = elementalCompositionHumanBody.H / elementalCompositionSolarSystem.H * 100 //Percentage
+   $('#hPercentageSolarSystem').find('p').text(hPercentageSolarSystem + "%");
+
+  var cTimesMilkyWay = elementalCompositionHumanBody.C / elementalCompositionMilkyWay.C  //Times
+    $('#cTimesMilkyWay').find('p').text(cTimesMilkyWay + " times more");  
+  var cTimesSolarSystem = elementalCompositionHumanBody.C / elementalCompositionSolarSystem.C  //Times
+    $('#cTimesSolarSystem').find('p').text(cTimesSolarSystem + " times more");  
+  
+  var nTimesMilkyWay = elementalCompositionHumanBody.N / elementalCompositionMilkyWay.N  //Times
+    $('#nTimesMilkyWay').find('p').text(nTimesMilkyWay + " times more");  
+
+  var nTimesSolarSystem = elementalCompositionHumanBody.N / elementalCompositionSolarSystem.N //Times
+    $('#nTimesSolarSystem').find('p').text(nTimesSolarSystem + " times more");  
+
+  var oTimesMilkyWay = elementalCompositionHumanBody.O / elementalCompositionMilkyWay.O  //Times
+    $('#oTimesMilkyWay').find('p').text(oTimesMilkyWay + " times more");  
+  var oTimesSolarSystem = elementalCompositionHumanBody.O / elementalCompositionSolarSystem.O  //Times
+    $('#oTimesSolarSystem').find('p').text(oTimesSolarSystem + " times more");  
+  var oTimesEarth = elementalCompositionHumanBody.O / elementalCompositionEarth.O  //Times
+    $('#oTimesEarth').find('p').text(oTimesEarth + " times more");  
+ 
+  var mgPercentageEarth = elementalCompositionHumanBody.Mg / elementalCompositionEarth.Mg * 100 //Percentage
+    $('#mgPercentageEarth').find('p').text(mgPercentageEarth + "%");  
+
+  var sPercentageEarth = elementalCompositionHumanBody.S / elementalCompositionEarth.S * 100 //Percentage
+      $('#sPercentageEarth').find('p').text(sPercentageEarth + "%");  
+
+  var caPercentageEarth = elementalCompositionHumanBody.Ca / elementalCompositionEarth.Ca * 100 //Percentage
+      $('#caPercentageEarth').find('p').text(caPercentageEarth + "%");  
+  // Divide your composition of things by the others to figure out how much more or less percent you have
+
+  var fePercentageMilkyWay = elementalCompositionHumanBody.Fe / elementalCompositionMilkyWay.Fe  * 100 // Percentage
+    $('#fePercentageMilkyWay').find('p').text(fePercentageMilkyWay + "%");  
+  var fePercentageEarth = elementalCompositionHumanBody.Fe / elementalCompositionEarth.Fe  * 100 // Percentage
+  $('#fePercentageEarth').find('p').text(fePercentageEarth + "%");  
+})
--- app/assets/javascripts/body_mass.js
+++ app/assets/javascripts/body_mass.js
@@ -1,68 +1,73 @@
 // Energy of a person
 
-var weight = 150;
-var mass = weight / 2.2;
+$(document).ready(function(){
 
-// You on planet but planet is moving
+  var weight = $('#weight').text();
+  var mass = weight / 2.2;
 
-var = ;
+  // You on planet but planet is moving
 
-var energyCalculation = function(mass, velocity){
-  var velocityMpS = velocity / 3.6;
-  energy = mass * Math.pow(velocityMpS, 2);
-  return energy;
-};
+  var energyCalculation = function(mass, velocity){
+    var velocityMpS = velocity / 3.6;
+    energy = 0.5 * mass * Math.pow(velocityMpS, 2);
+    return energy;
+  };
 
-var joulesToGallonOfGas = function(energy) {
-  return energy / 1.3 * Math.pow(10, 8);
-};
+  var joulesToGallonOfGas = function(energy) {
+    return energy / 1.3 * Math.pow(10, 8);
+  };
 
-var joulesToAABattery = function(energy) {
-  return energy / 1000;
-};
+  var joulesToAABattery = function(energy) {
+    return energy / 1000;
+  };
 
-var joulesToCandyBar = function(energy) {
-  return energy / 1000000;
-};
+  var joulesToCandyBar = function(energy) {
+    return energy / 1000000;
+  };
 
-var joulesToKgOfCoal = function(energy) {
-  return energy / 7.3 * Math.pow(10,6);
-};
+  var joulesToKgOfCoal = function(energy) {
+    return energy / 7.3 * Math.pow(10,6);
+  };
 
-var joulesToKgOfUranium = function(energy) {
-  return energy / 1.7 * Math.pow(10,13);
-};
+  var joulesToKgOfUranium = function(energy) {
+    return energy / 1.7 * Math.pow(10,13);
+  };
 
-var joulesToFoodCalories = function(energy) {
-  return energy / 4184;
-};
+  var joulesToFoodCalories = function(energy) {
+    return energy / 4184;
+  };
 
-var joulesToHiroshima = function(energy) {
-  return energy / Math.pow(10,14);
-};
+  var joulesToHiroshima = function(energy) {
+    return energy / Math.pow(10,14);
+  };
 
-var joulesToLighteningBolt = function(energy) {
-  return energy / Math.pow(10,10);
-};
+  var joulesToLighteningBolt = function(energy) {
+    return energy / Math.pow(10,10);
+  };
 
-var joulesSaturnV = function(energy) {
-  return energy / Math.pow(10,11);
-};
+  var joulesSaturnV = function(energy) {
+    return energy / Math.pow(10,11);
+  };
 
-var joulesKgOfTNT = function(energy) {
-  return energy / Math.pow(10,6);
-};
+  var joulesKgOfTNT = function(energy) {
+    return energy / Math.pow(10,6);
+  };
 
-var joulesHoppingFlee = function(energy) {
-  return energy / Math.pow(10,-7);
-};
+  var joulesHoppingFlee = function(energy) {
+    return energy / Math.pow(10,-7);
+  };
 
-// Come back and add latitude
-// 
-var earthRotating = energyCalculation(mass, 1600);
+  // Come back and add latitude
+  // 
+  var earthRotating = energyCalculation(mass, 1600);
 
-var earthOrbitingSun = energyCalculation(mass, 107000);
+  var earthOrbitingSun = energyCalculation(mass, 107000);
 
-var sunOrbitingGalaxy = energyCalculation(mass, 792000);
+  var sunOrbitingGalaxy = energyCalculation(mass, 792000);
+  
+  $('#earthRotating').find('p').text(earthRotating);
+  $('#earthOrbitingSun').find('p').text(earthOrbitingSun);
+  $('#sunOrbitingGalaxy').find('p').text(sunOrbitingGalaxy);
 
-// answers are in Joules
+  // answers are in Joules
+});
