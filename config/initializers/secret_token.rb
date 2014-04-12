f424bc4da0ddc3cb10a9391f1b088e05a584d580
Neal Peters <nealcpeters@gmail.com>
2014-04-12 11:20:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScienceHackDay::Application.config.secret_key_base = '1d801da61d56ec8daf941b7bb32d645ec204b1aa4325a6fc83ad0587def91aa80eba68fd69f8bea7c1f33e75a0cad58cc96b2e29efe1a6204fd134621e7b7432'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScienceHackDay::Application.config.session_store :cookie_store, key: '_science_hack_day_session'
