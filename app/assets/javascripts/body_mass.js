62611f21a5f243aaada7ddb92af2c70598164ca3
Neal Peters <nealcpeters@gmail.com>
2014-04-13 11:09:57 -0500

    spelling changes, fixed incorrect formulas, rounding

--- app/assets/javascripts/body_mass.js
+++ app/assets/javascripts/body_mass.js
@@ -9,23 +9,23 @@
   };
 
   var joulesToAABattery = function(energy) {
-    return energy / 1000;
+    return (energy / 1000).toFixed(2);
   };
 
   var joulesToCandyBar = function(energy) {
-    return energy / 1000000;
+    return (energy / 1000000).toFixed(2);
   };
 
   var joulesToKgOfCoal = function(energy) {
-    return energy / 7.3 * Math.pow(10,6);
+    return (energy / 7.3 * Math.pow(10,6)).toFixed(2);
   };
 
   var joulesToKgOfUranium = function(energy) {
-    return energy / 1.7 * Math.pow(10,13);
+    return (energy / 1.7 * Math.pow(10,13)).toFixed(2);
   };
 
   var joulesToFoodCalories = function(energy) {
-    return energy / 4184;
+    return (energy / 4184).toFixed(2);
   };
 
   var joulesToHiroshima = function(energy) {
@@ -41,11 +41,11 @@
   };
 
   var joulesToKgOfTNT = function(energy) {
-    return energy / Math.pow(10,6);
+    return (energy / Math.pow(10,6)).toFixed(2);
   };
 
   var joulesToHoppingFlea = function(energy) {
-    return energy / Math.pow(10,-7);
+    return (energy / Math.pow(10,-7)).toFixed(2);
   };
 // Energy of a person
 
@@ -94,7 +94,7 @@ $(document).ready(function(){
   threeSet("joulesToCandyBar", "Candy Bars")
   threeSet("joulesToKgOfCoal", "Kilograms of Coal") 
   threeSet("joulesToKgOfUranium", "Kilograms of Uranium") 
-  threeSet("joulesToFoodCalories", "Foord Calories")     
+  threeSet("joulesToFoodCalories", "Food Calories")     
   threeSet("joulesToHiroshima", "Hiroshima Atom Bombs")  
   threeSet("joulesToLighteningBolt", "Bolts of Lightening")     
   threeSet("joulesToSaturnV", "Saturn V Rockets")          
