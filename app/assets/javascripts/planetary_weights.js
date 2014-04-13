740b72e3a2cb8339f271d1b9dcc597b31fddb9f9
Eric Stensland <estensland88@gmail.com>
2014-04-12 19:43:01 -0500

    mew branch commit

--- app/assets/javascripts/planetary_weights.js
+++ app/assets/javascripts/planetary_weights.js
@@ -1,10 +1,14 @@
 relativeWeights = {
-  Mercury : .38,
-  Venus : .90,
-  Mars : .38,
+  Mercury : 0.38,
+  Venus : 0.90,
+  Mars : 0.38,
   Jupiter : 2.87,
   Saturn : 1.32,
-  Uranus : .93,
+  Uranus : 0.93,
   Neptune : 1.23,
-  Pluto : .07
-  }
+  Pluto : 0.07
+  };
+
+var weight = $('#weight').text();
+
+$('#lifeStarRange').text(relativeWeights[parseInt(weight)] + " stars");
